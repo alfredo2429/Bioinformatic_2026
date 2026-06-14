e24b57ad076a990964a76aa2ff88f962582036e8
alfredo2429 <md.alfredocardenas@gmail.com>
2026-06-13 21:37:16 -0500

    Edit Spike sequences

--- scr/SpikeAnalysis.R
+++ scr/SpikeAnalysis.R
@@ -59,7 +59,7 @@ web_history_obj <- search$web_history # define "history"
 library(progress)
 pb <- progress_bar$new(
   format = "Batch :current/:total [:bar] :percent | Elapsed: :elapsed | ETA: :eta",
-  total = length(seq(0, 1900, by = 50)),
+  total = length(seq(0, 2000, by = 50)),
   clear = FALSE,
   width = 90
 )
@@ -70,7 +70,7 @@ outfile <- file.path(getwd(),
                      "Spike2000.fasta")
 
 
-for(start in seq(0, 1900, by = 50)) {
+for(start in seq(0, 2000, by = 50)) {
   # Download fasta files (batch)
   fasta <- entrez_fetch(
     db = "protein",
@@ -106,6 +106,11 @@ typeof(seqs)
 class(seqs)
 summary(seqs)
 
+# control quality
+ambiguous <- sapply(seqs,function(x){sum(strsplit(as.character(x),"")[[1]]%in%c("X","B","Z","J","*"))})
+seqs <- seqs[ambiguous < 1]
+length(seqs)
+
 # remove duplicates
 seqs <- seqs[!duplicated(as.character(seqs))]
 length(seqs)
@@ -127,21 +132,21 @@ ggplot( len, aes(x=x)) +
     plot.title = element_text(size=15)
   )
 # Remove outliers
+seqs <- seqs[
+  len$x > 1250 &
+    len$x < 1290
+]
+length(seqs)
+
 p <- boxplot(len$x)
 p$out
-
-summary(len$x)
-
-seqs <- seqs[
-  len$x > 1220 &
-    len$x < 1320
-]
-
+seqs <- seqs[-p$out]
 length(seqs)
+
 len <- data.frame(x=width(seqs))
 summary(len$x)
 ggplot( len, aes(x=x)) +
-  geom_histogram( binwidth=3, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  geom_histogram( binwidth=2, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
   ggtitle("SPIKE") +
   labs(subtitle = "(Sequence legnths)") +
   theme_ipsum() +
@@ -149,30 +154,54 @@ ggplot( len, aes(x=x)) +
     plot.title = element_text(size=15)
   )
 
+
 # SEQUENCE ALIGNMENT ===================
-alignment <- AlignSeqs(
+library(msa)
+alignment <- msa(
   seqs,
-  processors = NULL
+  method = "ClustalOmega", # Omega more efficient for >1000 seq
+  verbose = TRUE
 )
-writeXStringSet(
+
+library(Biostrings)
+aligned <- msaConvert(
   alignment,
+  type = "seqinr::alignment"
+)
+
+# Evaluate alignment quality
+library(DECIPHER)
+alignment2 <- AAStringSet(alignment)
+BrowseSeqs(alignment2)
+
+# save
+writeXStringSet(
+  alignment2,
   filepath = file.path(getwd(),"result","Spike_alignment.fasta")
 )
 
+
+# Calculated gaps
+mat <- as.matrix(alignment2)
+gap_fraction <- apply(mat,1,function(x) mean(x=="-"))
+hist(gap_fraction)
+summary(gap_fraction)
+
+# remove sequence with many gaps
+keep <- gap_fraction < 0.0731
+alignment2 <- alignment2[keep,]
+BrowseSeqs(alignment2)
+
+
 # CONCESUS SEQUENCE =============
 consensus <- ConsensusSequence(
-  alignment
-)
-
-writeLines(
-  as.character(consensus),
-  file.path(getwd(),"result","Spike_consensus.fasta")
+  alignment2
 )
 
 # PHYLOGENY ==============================
 
 # transform data
-mat <- as.matrix(alignment)
+mat <- as.matrix(alignment2)
 
 phy <- phyDat(
                mat,
@@ -185,25 +214,117 @@ dm <- dist.ml(phy)
 # Neightbor join tree
 tree <- NJ(dm)
 tree <- midpoint(tree) # root the midpoint
+
+# Optimize tree
+fit <- pml(tree,phy)
+fit <- optim.pml(
+  fit,
+  model="JTT",
+  optGamma=TRUE,
+  optInv=TRUE
+)
+
+# best tree
+tree.ml <- fit$tree
+
+# plot
+library(ggtree)
+ggtree(tree)
+ggtree(tree.ml)
+
+
+# Bootstrap tree (take hours)
+# bs <- bootstrap.pml(fit,
+#                     bs=100,
+#                     optNni=TRUE
+#                     )
+
+
 write.tree(            # save the tree
   tree,
-  file = "Spike_tree.nwk"
+  file = file.path(getwd(),"result","NJ_Spike_tree.nwk")
+)
+
+write.tree(            # save the tree
+  tree.ml,
+  file = file.path(getwd(),"result","LH_Spike_tree.nwk")
 )
 
 # plot phylogeny tree
 pdf(
-  "Spike_phylogeny.pdf",
-  width = 14,
-  height = 14
+  file.path(getwd(),"plot","Spike_phylogeny.pdf"),
+  width = 60,
+  height = 60
 )
 
-plot(
-  tree,
-  cex = 0.15,
-  no.margin = TRUE
-)
+ggtree(tree)
+ggtree(tree.ml)
 
 dev.off()
+
+# IDENTIFY CLADES ==============================
+class(tree.ml)
+class(tree)
+class(fit)
+class(bs)
+
+# convert distance
+cophenetic_matrix <- cophenetic(tree.ml)
+
+# hierarchical clustering
+hc <- hclust(as.dist(cophenetic_matrix))
+
+# Cluster identify
+library(cluster)
+
+for(k in 2:10){
+  cl <- cutree(hc,k)
+  sil <- silhouette(cl,dist(cophenetic_matrix))
+  print(c(k,mean(sil[,3])))
+}
+
+# print tree
+BiocManager::install("ggtree")
+library(ggtree)
+
+p <- ggtree(tree_ml)
+p +
+  geom_tiplab(size=2) +
+  theme_tree2()
+p +
+  geom_tippoint(
+    aes(color=factor(clades)),
+    size=2
+  )
+
+# save
+ggsave(
+  file.path(getwd(),"plot","Spike_phylogeny.pdf"),
+  width=12,
+  height=10
+)
+
+# PLOT ALIGNMENT ================================
+library(ggmsa)
+ggmsa(
+  alignment_filtered,
+  start = 1,
+  end = 200
+)
+
+# Conservation map
+library(msa)
+msaPrettyPrint(
+  alignment,
+  output="pdf",
+  file= file.path(getwd(),"plot","alignment.pdf")
+)
+
+# LOGO plot
+library(ggseqlogo)
+mat <- as.matrix(alignment_filtered)
+
+ggseqlogo(mat)
 
 # VARIABLE SITE ANALYSIS =========================
 n_sites <- ncol(mat)
